a2ab42b269b4cc5c2fb30b4ce0ae3015349deaba
AmyDeveloperr <amar321dombe@gmail.com>
2022-10-15 17:06:11 +0530

    Task 7_sent response from server

--- app.js
+++ app.js
@@ -16,8 +16,33 @@ const http = require('http');
 
 //with arrow
 
+//sending response 
+
 const server = http.createServer((req, res) => {
-    console.log('amar');
+    console.log(req.url, req.method, req.headers);
+
+    if(req.url === '/home') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title> My first page</title></head>');
+        res.write('<body><h1>Welcome Home</h1></body>');
+        res.write('</html>')
+        res.end(); 
+    } else if(req.url === '/about') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title> My first page</title></head>');
+        res.write('<body><h1>Welcome to about us page</h1></body>');
+        res.write('</html>')
+        res.end(); 
+    } else if(req.url === '/node') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title> My first page</title></head>');
+        res.write('<body><h1>Welcome to my Node JS project</h1></body>');
+        res.write('</html>')
+        res.end(); 
+    }
 })
 
 server.listen(4000);
