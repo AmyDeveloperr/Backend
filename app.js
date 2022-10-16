48e78004e62f84d3b50fba1dc4875c95856e24eb
AmyDeveloperr <amar321dombe@gmail.com>
2022-10-16 15:14:20 +0530

    clean code by importing code from routes.js

--- app.js
+++ app.js
@@ -1,48 +1,19 @@
 
+//clean code
 //creating server
 const http = require('http');
 
-// function rqListener(req, res) {
+const routes = require('./routes.js')
 
-// }
+console.log(routes.someText);
 
-// http.createServer(rqListener);
-
-// //with anonymous function 
-
-// http.createServer(function rqListener(req, res) {
-
-// })
-
-//with arrow
 
 //sending response 
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-
-    if(req.url === '/home') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title> My first page</title></head>');
-        res.write('<body><h1>Welcome Home</h1></body>');
-        res.write('</html>')
-        res.end(); 
-    } else if(req.url === '/about') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title> My first page</title></head>');
-        res.write('<body><h1>Welcome to about us page</h1></body>');
-        res.write('</html>')
-        res.end(); 
-    } else if(req.url === '/node') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title> My first page</title></head>');
-        res.write('<body><h1>Welcome to my Node JS project</h1></body>');
-        res.write('</html>')
-        res.end(); 
-    }
-})
+const server = http.createServer(routes.handler);
 
 server.listen(4000);
+
+
+
+
