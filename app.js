1f2be545e7ca2c7f06225d5dfc592f7b854ebad2
AmyDeveloperr <amar321dombe@gmail.com>
2022-10-17 15:24:57 +0530

    cleaning code using routers

--- app.js
+++ app.js
@@ -2,25 +2,19 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+
 const app = express();
 
-
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('/admin', adminRoutes);
+app.use('/shop', shopRoutes);
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
-    
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>');
 })
 
-app.post('/product',(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express</h1>');   
-})
 
   
 app.listen(3000);
