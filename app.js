f0ecb70fda68a5fdbbe8b74a673c02ea1fda7cd3
AmyDeveloperr <amar321dombe@gmail.com>
2022-10-23 03:31:15 +0530

    testing

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
 
+
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const addProductRoutes = require('./routes/add-product.js');
-const shopRoutes = require('./routes/shop.js');
-const contactRoutes = require('./routes/contact-us.js');
-const successRoutes = require('./routes/success.js');
-// const loginRoutes = require('./routes/login.js');
-// const messageRoutes = require('./routes/message.js');
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const contactRoutes = require('./routes/contact-us.js');
+const successRoutes = require('./routes/success.js');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(addProductRoutes);
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(contactRoutes);
 app.use(successRoutes);
-// app.use(loginRoutes);
-// app.use(messageRoutes);
 
+app.use(errorController.get404);
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'pageNotFound.html')); 
-});
-// app.use((req, res, next) => {
-//     res.status(404).send('<h1>Page Not Found</h1>');
-// })
 app.listen(3000);
-
 
 
 
