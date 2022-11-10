0e6c0bdbbd0d356b9f07b1584d7523457368966d
AmyDeveloperr <amar321dombe@gmail.com>
2022-11-11 00:04:49 +0530

    backend login

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 
 const User = require('../models/user')
+
 exports.addUserDetails = async (req, res, next) => {
     try {
     if(!req.body.emailId) {
@@ -17,17 +18,52 @@ exports.addUserDetails = async (req, res, next) => {
     const email = req.body.emailId;
     const name = req.body.userName;
     const pass = req.body.pass;
+    
+    const dbData =  await User.findAll({where:{email}});
 
-    const dbData = await User.findAll({where:{email:email}});
-
-    console.log('this is db data', dbData);
-
-    if (email === dbData) {
-        res.status(500).json({message:'user already exists'});
-    } else {
+    //console.log('this is db data', dbData[0].email);
+    if(dbData.length > 0) {
+        
+        res.status(207).json({message:'user already exists'});     
+    }
+    else {
         await User.create({email: email, username: name, password: pass});
         res.status(201).json({message: 'user successfully created'});
     }
+      
+}catch(err) {
+    res.status(500).json({error: err});  
+  }
+
+};
+
+exports.addLoginDetails = async (req, res, next) => {
+
+    try {
+
+        if(!req.body.email) {
+            throw new Error('Email is mandatory');
+        }
+
+        if(!req.body.pass) {
+            throw new Error('password is mandatory');
+        }
+
+        const pass = req.body.pass;
+        const email = req.body.email;
+        
+        const dbData = await User.findAll({where:{email}});
+        if(dbData.length > 0) {
+
+            if (dbData[0].password == pass) {
+                res.status(201).json({message:'user login successful'});
+            } 
+            else {
+                return res.status(207).json({message:'Password is wrong'});
+            }
+        } else {
+            return res.status(208).json({message:'user does not exist'});
+        }
 
 }catch(err) {
     res.status(500).json({error: err});
--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,8 @@ const userController = require('../controllers/user');
 
 const router = express.Router();
 
-router.post('/user/signup', userController.addUserDetails); 
+router.post('/signup', userController.addUserDetails); 
+
+router.post('/login', userController.addLoginDetails);
+
 module.exports = router;
