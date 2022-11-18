344a6526bbbc6f307e2931655ab1439c466f48f1
AmyDeveloperr <amar321dombe@gmail.com>
2022-11-18 19:49:26 +0530

    resetting-updating the password

--- controllers/forgotPass.js
+++ controllers/forgotPass.js
@@ -1,5 +1,6 @@
 const uuid = require('uuid');
 const sgMail = require('@sendgrid/mail');
+const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const Forgotpassword = require('../models/forgotpassword');
 require('dotenv').config();
@@ -25,13 +26,13 @@ exports.forgotPassword = async (req, res) => {
                 html: `<a href="http://localhost:3000/password/resetpassword/${id}">Reset password</a>`,
             }
 
-            sgMail
+            sgMail  
             .send(msg)
             .then((response) => {
 
                 // console.log(response[0].statusCode)
                 // console.log(response[0].headers)
-                return res.status(response[0].statusCode).json({message: 'Link to reset password sent to your mail ', sucess: true})
+                return res.status(202).json({message: 'Link to reset password sent to your mail ', sucess: true})
 
             })
             .catch((error) => {
@@ -45,6 +46,70 @@ exports.forgotPassword = async (req, res) => {
     } catch(err){
         console.error(err)
         return res.json({ message: err, sucess: false });
+    }
+
+}
+
+exports.resetpassword = (req, res) => {
+    const id =  req.params.id;
+    Forgotpassword.findOne({ where : { id }}).then(forgotpasswordrequest => {
+        if(forgotpasswordrequest){
+            forgotpasswordrequest.update({ active: false});
+            res.status(200).send(`<html>
+                                    <script>
+                                        function formsubmitted(e){
+                                            e.preventDefault();
+                                            console.log('called')
+                                        }
+                                    </script>
+                                    <form action="/password/updatepassword/${id}" method="get">
+                                        <label for="newpassword">Enter New password</label>
+                                        <input name="newpassword" type="password" required></input>
+                                        <button>reset password</button>
+                                    </form>
+                                </html>`
+                                )
+            res.end()
+
+        }
+    })
+}
+
+exports.updatepassword = (req, res) => {
+
+    try {
+        const { newpassword } = req.query;
+        const { resetpasswordid } = req.params;
+        Forgotpassword.findOne({ where : { id: resetpasswordid }}).then(resetpasswordrequest => {
+            User.findOne({where: { id : resetpasswordrequest.userId}}).then(user => {
+                // console.log('userDetails', user)
+                if(user) {
+                    //encrypt the password
+
+                    const saltRounds = 10;
+                    bcrypt.genSalt(saltRounds, function(err, salt) {
+                        if(err){
+                            console.log(err);
+                            throw new Error(err);
+                        }
+                        bcrypt.hash(newpassword, salt, function(err, hash) {
+                            // Store hash in your password DB.
+                            if(err){
+                                console.log(err);
+                                throw new Error(err);
+                            }
+                            user.update({ password: hash }).then(() => {
+                                res.status(201).json({message: 'Successfuly updated the new password'})
+                            })
+                        });
+                    });
+            } else{
+                return res.status(404).json({ error: 'No user Exists', success: false})
+            }
+            })
+        })
+    } catch(error){
+        return res.status(403).json({ error, success: false } )
     }
 
 }
--- routes/forgotPass.js
+++ routes/forgotPass.js
@@ -6,4 +6,8 @@ const forgotPassController = require('../controllers/forgotPass');
 
 router.post('/forgotpassword', forgotPassController.forgotPassword );
 
+router.get('/resetpassword/:id', forgotPassController.resetpassword);
+
+router.get('/updatepassword/:resetpasswordid', forgotPassController.updatepassword);
+
 module.exports = router;
