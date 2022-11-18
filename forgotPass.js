a7cd2e3ae1e9dd9d3e47546ba8cf7a7e59dd73a1
AmyDeveloperr <amar321dombe@gmail.com>
2022-11-18 19:51:18 +0530

    reset-update password

--- forgotPass.js
+++ forgotPass.js
@@ -3,25 +3,26 @@ const forgotPass = document.getElementById('forgotPassForm');
 
 forgotPass.addEventListener('submit', forgotPassword);
 
-function forgotPassword(e) {
+async function forgotPassword(e) {
     e.preventDefault();
-
-    const form = new formData(e.target);
+    console.log(e.target.name)
+    //const form = new formData(e.target);
+    const email = e.target.email.value;
 
     const userDetails = {
-        email: form.get('email')
+        email
     }
     try {
-    const response = axios.post('http://localhost:3000/password/forgotpassword',userDetails);
-
+    const response = await axios.post('http://localhost:3000/password/forgotpassword',userDetails);
+        console.log(response);
         if(response.status === 202){
-            document.body.innerHTML += '<div style="color:red;">Mail Successfuly sent <div>'
-        } else {
-            throw new Error('Something went wrong!!!')
-        }
+            document.body.innerHTML += '<div style="color:yellow;">Mail Successfuly sent <div>'
+        } 
     }catch(err) {
         document.body.innerHTML += `<div style="color:red;">${err} <div>`;
 
     }
 
 }
+
+
